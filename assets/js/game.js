b9fee9f513b6dcfd9f242d3f831fa0d908c58845
JayP4801 <jpham4801@gmail.com>
2022-03-06 00:33:01 -0500

    done

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var prompt = function(){
+    window.alert("ONLY");
+    window.alert("TEN");
+    window.alert("MORE");
+    window.alert("DAYS");
+    window.alert("UNTIL");
+    window.alert("I");
+    window.alert("GET");
+    window.alert("TO");
+    window.alert("SEE");
+    window.alert("YOUR");
+    window.alert("FACE");
+    window.alert("❤️");
+}
+
+prompt();
